d42f6b535757a5ec1bca724c35beec612cbf495f
Rafael_Madureira <rafinhakd4@gmail.com>
2024-11-06 21:43:59 -0300

    feat 1_implementation_setup

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Course from "./Classes/courses";
+
+const c = new Course("ReactJS","Um curso voltado para qualquer pessoa que deseja ingressar na areá da computação","Pedro Leal","30 horas","5");
+
+console.log("Voce esta procurando pelo curso ")
